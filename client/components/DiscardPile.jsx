b4f0e8170d0ce86617ffd8754dac6b2238384d77
EddieKeller <116ekg@gmail.com>
2017-08-09 12:32:06 -0700

    image and style changes minor

--- client/components/DiscardPile.jsx
+++ client/components/DiscardPile.jsx
@@ -4,9 +4,9 @@ const DiscardPile = (props) => {
   //console.log('these are the DiscardPile props', props.discard)
   let lastCard = props.discard[props.discard.length - 1] ||
      [{ 
-       image: 'https://hearthstone.gamepedia.com/media/hearthstone.gamepedia.com/thumb/4/4c/Card_back-Pandaria.png/200px-Card_back-Pandaria.png?version=c5a8d433de2125b5a6751a8090a7e241',
-       name: 'none',
-       description: 'i-am-a-placholder'
+       image: 'http://acedmagazine.com/wp-content/uploads/catsleeping.jpg',
+       name: 'Discard Pile',
+       description: 'Discard Pile'
     }]
   //console.log(lastCard[0])
       
