3d5974c7d6924ed413e0d9a052cb4453c8e6fef5
mkdai <michaelkdai@gmail.com>
2017-08-10 16:17:32 -0700

    separate branch for deployment

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -1,11 +1,11 @@
 import auth0 from 'auth0-js';
-if(!process.env.DOMAIN || !process.env.CLIENT_ID || !process.env.REDIRECT_URI || !process.env.AUDIENCE){
-    import config from './config.js';
-}
-let DOMAIN = process.env.DOMAIN || config.DOMAIN
-let CLIENT_ID = process.env.CLIENT_ID || config.CLIENT_ID
-let REDIRECT_URI = process.env.REDIRECT_URI || config.REDIRECT_URI
-let AUDIENCE = process.env.AUDIENCE || config.AUDIENCE
+// if(!process.env.DOMAIN || !process.env.CLIENT_ID || !process.env.REDIRECT_URI || !process.env.AUDIENCE){
+//     import config from './config.js';
+// }
+let DOMAIN = process.env.DOMAIN //|| config.DOMAIN
+let CLIENT_ID = process.env.CLIENT_ID //|| config.CLIENT_ID
+let REDIRECT_URI = process.env.REDIRECT_URI //|| config.REDIRECT_URI
+let AUDIENCE = process.env.AUDIENCE //|| config.AUDIENCE
 
 export default class Auth {
     constructor() {
