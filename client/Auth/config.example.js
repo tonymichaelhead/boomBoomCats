78e1dc52f9c2501efa2dd2dd243e5083ee838a6a
mkdai <michaelkdai@gmail.com>
2017-08-10 14:59:46 -0700

    added auth config.example

---
+++ client/Auth/config.example.js
@@ -0,0 +1,6 @@
+export default {
+    CLIENT_ID: 'Bt93UxMPclylp6P1iwcOY6ofPQsNeSZo',
+    DOMAIN: 'michaelkdai.auth0.com',
+    REDIRECT_URI: 'http://localhost:3000/',
+    AUDIENCE: 'https://michaelkdai.auth0.com/userinfo'
+}
