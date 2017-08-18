0974c75cb5b1b5d8f314e264596d7dcddd8cfa87
jchandr1 <justinchands@gmail.com>
2017-08-17 18:53:32 -0700

    setting stuff upt

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -16,6 +16,7 @@ export default class Game extends React.Component {
     this.drawACard = this.drawACard.bind(this)
     this.handleDeckClick = this.handleDeckClick.bind(this)
     this.handleCardClick = this.handleCardClick.bind(this)
+    this.changeTheFuture = cardFunctions.changeTheFuture.bind(this)
     this.state = {
       allPlayersId: [],
       playerId: '',
@@ -60,6 +61,12 @@ export default class Game extends React.Component {
       })
       console.log('we shuffled the deck guys')
     }.bind(this))
+
+    this.props.socket.on('change order', function(deck) {
+      this.setState({
+        deck: deck
+      })
+    })
 
     this.props.socket.on('saw future', function(player) {
       console.log(player, ' saw the future of the deck!')
@@ -111,7 +118,6 @@ export default class Game extends React.Component {
 
       this.attackNextPlayer(handIndex, ()=>{this.props.socket.emit('attack card', this.state.turn, this.state.exploderCount)});
       
-
     } else if ( cardName === 'shuffle') {
 
       this.shuffleDeck(handIndex,()=>{
